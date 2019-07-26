353756c948888e19d165f6607208ce15d8d43891
Ubuntu <ubuntu@ip-172-31-61-43.us-west-2.compute.internal>
2019-07-26 20:50:41 +0000

    updates to build_space_records.py

---
+++ build_space_records.py
@@ -0,0 +1,135 @@
+# import the necessary packages
+from config import space_config as config
+from utils.tfannotation import TFAnnotation
+from sklearn.model_selection import train_test_split
+from PIL import Image
+import tensorflow as tf
+import os
+import json
+import glob
+import csv
+import numpy as np
+import cv2
+
+
+def main(_):
+        # open the classes output file
+        f = open(config.CLASSES_FILE, "w")
+
+        # loop over the classes
+        for (k, v) in config.CLASSES.items():
+                # construct the class information and write to file
+                item = ("item {\n"
+                                "\tid: " + str(v) + "\n"
+                                "\tname: '" + k + "'\n"
+                                "}\n")
+                f.write(item)
+
+        # close the output classes file
+        f.close()
+
+        # initialize a data dictionary used to map each image filename
+        # to all bounding boxes associated with the image, then load
+        # the contents of the annotations file
+D = {}
+labels = {}
+with open('/home/ubuntu/satellite_vehicle_count/xview_class_labels.txt') as f:
+    for row in csv.reader(f):
+        labels[int(row[0].split(":")[0])] = row[0].split(":")[1]
+
+with open('/home/ubuntu/satellite_vehicle_count/xView_train.geojson') as f:
+    data = json.load(f)
+
+        # loop over each object in the annotation json
+for row in range(len(data['features'])):
+    # parse through
+    startX, startY, endX, endY = [int(x) for x in data['features'][row]['properties']['bounds_imcoords'].split(',')]
+    imagePath = data['features'][row]['properties']['image_id']
+    label = data['features'][row]['properties']['type_id']
+    if label in labels.keys():
+        label = labels[label]
+    else:
+        continue
+ # if we are not interested in the label, ignore it
+    if label not in config.CLASSES:
+        continue
+    # build the path to the input image, then grab any other
+    # bounding boxes + labels associated with the image, path,
+    # labels and bounding box lists, repsectively
+    p = os.path.sep.join([config.BASE_PATH,"train_images/{}".format(imagePath)])
+
+    b = D.get(p, [])
+
+    # build a tuple consisting of the label and bounding box,
+    # then update the list and store it in the dictionary
+    b.append((label, (startX, startY, endX, endY)))
+    D[p] = b
+
+    # create training and testing splits from our data dictionary
+    (trainKeys, testKeys) = train_test_split(list(D.keys()),
+            test_size=config.TEST_SIZE, random_state=42)
+
+    # initalize the data split files
+    datasets = [
+        ("train", trainKeys, config.TRAIN_RECORD),
+        ("test", testKeys, config.TEST_RECORD)
+    ]
+    
+# loop over the datasets
+for (dType, keys, outputPath) in datasets:
+    # initalize the Tensorflow writer and initalize the total
+    # number of examples written to file
+    print("[INFO] processing '{}'...".format(dType))
+    writer = tf.python_io.TFRecordWriter(outputPath)
+    total = 0
+
+    # loop over all the keys in the current set
+    for k in keys:
+        # load the input image from disk as TensorFlow object
+        encoded = tf.gfile.GFile(k, "rb").read()
+        encoded = bytes(encoded)
+
+        # load the image from disk again, this time as a PIL object
+        pilImage = Image.open(k)
+        (w, h) = pilImage.size[:2]
+
+        # parse the filename and encoding from the input path
+        filename = k.split(os.path.sep)[-1]
+        encoding = filename[filename.rfind(".") + 1:]
+
+        # initalize the annotation object used to store
+        # information regarding the bounxing box + labels
+        tfAnnot = TFAnnotation()
+        tfAnnot.image = encoded
+        tfAnnot.encoding = encoding
+        tfAnnot.filename = filename
+        tfAnnot.width = w
+        tfAnnot.height = h
+
+        # loop over the bounding boxes + labels assocaited with the image
+        for (label, (startX, startY, endX, endY)) in D[k]:
+            # Tensorflow assumes all bounxing boxes are in the
+            # range [0, 1] so we need to scale them
+            xMin = startX / w
+            xMax = endX / w
+            yMin = startY / h
+            yMax = startY / h
+
+            # load the input image from disk and denormalize the
+            # bounding box coordinates
+            image = cv2.imread(k)
+            startX = int(xMin * w)
+            startY = int(yMin * h)
+            endX = int(xMax * w)
+            endY = int(yMax *h)
+
+            print("[INFO] Drawing Boxes on Images")
+            # draw the bounding boxes on the image
+            cv2.rectangle(image, (startX, startY), (endX, endY),
+                           (0, 255, 0), 2)
+            cv2.imwrite('/home/ubuntu/satellite_vehicle_count/test_images/{}'.format(
+                    filename), image)
+
+# check to see if the main thread should be started
+if __name__ == "__main__":
+        tf.app.run()
--- config/space_config.py
+++ config/space_config.py
@@ -1,21 +1,23 @@
 # import the necessary packages
 import os
 
-# intialize the base path for the Xview Dataset
-BASE_PATH = "capstone"
+# initialize the base path for the Xview dataset
+BASE_PATH = "/home/ubuntu/satellite_vehicle_count"
 
 # build the path to the annotations file
-
 ANNOT_PATH = os.path.sep.join([BASE_PATH, "xView_train.geojson"])
 
-# build the path to the output training and testing record files,
-# along with the class labels file
+# build the path to the ouput training and testing record files,
+# along with the class labels files
 TRAIN_RECORD = os.path.sep.join([BASE_PATH,
     "records/training.record"])
 TEST_RECORD = os.path.sep.join([BASE_PATH,
     "records/testing.record"])
-CLASSES_FILES = os.path.sep.join([BASE_PATH,
+CLASSES_FILE = os.path.sep.join([BASE_PATH,
     "records/classes.pbtxt"])
 
+# initalize the test split size
+TEST_SIZE = 0.25
+
 # initalize the class labels dictionary
-CLASSES = {
+CLASSES = {"Passenger Vehicle":1,"Small Car":2, "Bus":3, "Pickup Truck":4,"Utility Truck":5,"Truck": 6, "Cargo Truck":7, "Truck w/Box":8, "Truck w/Flatbed" :9, "Building": 10, "Vehicle Lot":11, "Damaged Building": 12}
